c96f7c633cb81c64a2fe0a062f27bc3bd7d487ba
session <252201310+session567@users.noreply.github.com>
2026-02-04 20:02:37 +0100

    Add stylelint

---
+++ stylelint.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import("stylelint").Config} */
+export default {
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'selector-class-pattern': null,
+    'at-rule-no-unknown': [true, { ignoreAtRules: ['for'] }],
+  },
+}
